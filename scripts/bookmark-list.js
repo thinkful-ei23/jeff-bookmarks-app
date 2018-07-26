a9688aa46f5d14ba125addb24c413945680e998f
maxjeffwell <maxjeffwell@gmail.com>
2018-07-26 17:24:57 -0400

    finished index.js

--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -1,5 +1,46 @@
 'use strict';
 // eslint-disable-next-line no-unused-vars
-/* global $ */
+/* global $, api, store */
+
+const myList = (function() {
+  const createInputControls = function() {
+    `INSERT HTML HERE`;
+  };
+
+  const createForm = function() {
+    const hiddenIfNotAdding = function() {
+      if (store.adding) {
+        return 'hidden';
+      }else {
+        return '';
+      } 
+    }; 
+  };
+
+  const createToast = function() {
+    const errorMessage = '';
+    if (store.error) {
+      errorMessage = `INSERT HTML HERE`;
+    } 
+    return errorMessage;
+  }; 
+
+  return `INSERT HTML HERE`;
+
+  const renderMyList = function() {
+    const bookmarks = store.bookmarks;
+    let filteredItems = bookmarks.filter((bookmark) => {
+      bookmark.rating >= store.filter;
+  }); 
+
+
+
+
+
+});
+
+
+
+
 
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
 'use strict';
 // eslint-disable-next-line no-unused-vars
-/* global $ */
+/* global $, myList, store, api */
 
 $(function() {
   myList.bindEventListeners();
   myList.render();
 
   api.getBookmark((bookmarks) => {
-    bookmarks.forEach((bookmark) => store.addBookmark(bookmark));
+    bookmarks.forEach((bookmark) => { 
+      store.addBookmark(bookmark);
+    });
     myList.render();
   });
 });
